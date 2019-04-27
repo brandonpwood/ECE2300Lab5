b1a7ef425f7019c0fe3f4184205861dede3bba62
jl3253 <49843821+jl3253@users.noreply.github.com>
2019-04-27 17:07:42 -0400

    Update adder.v

--- adder.v
+++ adder.v
@@ -1,93 +1,30 @@
-module adder(A,B,CI,Y,C,V); // add all inputs and outputs inside parentheses
+module adder(A, B, CI, Y, C, V); // add all inputs and outputs inside parentheses
 
   // inputs
   input [7:0] A;
   input [7:0] B;
   input CI;
-  
+
   // outputs
   output [7:0] Y;
   output C;
   output V;
-  
-  
+
   // reg and internal variable definitions
-  
+  wire carry[7:0];
   
   // implement module here
-	
-	wire C1;
-	wire C2;
-	wire C3;
-	wire C4;
-	wire C5;
-	wire C6;
-	wire C7;
-	wire C8;
-	wire Y0;
-	wire Y1;
-	wire Y2;
-	wire Y3;
-	wire Y4;
-	wire Y5;
-	wire Y6;
-	wire Y7;
-	onebit first(
-		.A(A[0]),
-		.B(B[0]),
-		.CIN(CI),
-		.O(Y0),
-		.COUT(C1)
-	);
-	onebit second(
-		.A(A[1]),
-		.B(B[1]),
-		.CIN(C1),
-		.O(Y1),
-		.COUT(C2)
-	);
-   onebit third(
-		.A(A[2]),
-		.B(B[2]),
-		.CIN(C2),
-		.O(Y2),
-		.COUT(C3)
-	);
-	onebit fourth(
-		.A(A[3]),
-		.B(B[3]),
-		.CIN(C3),
-		.O(Y3),
-		.COUT(C4)
-	);
-	onebit fifth(
-		.A(A[4]),
-		.B(B[4]),
-		.CIN(C4),
-		.O(Y4),
-		.COUT(C5)
-	);
-	onebit sixth(
-		.A(A[5]),
-		.B(B[5]),
-		.CIN(C5),
-		.O(Y5),
-		.COUT(C6)
-	);
-	onebit seventh(
-		.A(A[6]),
-		.B(B[6]),
-		.CIN(C6),
-		.O(Y6),
-		.COUT(C7)
-	);
-	onebit eighth(
-		.A(A[7]),
-		.B(B[7]),
-		.CIN(C7),
-		.O(Y7),
-		.COUT(C)
-	);
-	assign V = ((A[7] & B[7] & ~Y[7]) | (~A[7] & ~B[7] & Y[7])) ? 1'b1 : 1'b0;
-	assign Y = {Y7, Y6, Y5, Y4, Y3, Y2, Y1, Y0};
+  fullAdder A0(.A(A[0]), .B(B[0]), .Cin(CI), .Cout(carry[0]), .S(Y[0]) );
+  fullAdder A1(.A(A[1]), .B(B[1]), .Cin(carry[0]), .Cout(carry[1]), .S(Y[1]) );
+  fullAdder A2(.A(A[2]), .B(B[2]), .Cin(carry[1]), .Cout(carry[2]), .S(Y[2]) );
+  fullAdder A3(.A(A[3]), .B(B[3]), .Cin(carry[2]), .Cout(carry[3]), .S(Y[3]) );
+  fullAdder A4(.A(A[4]), .B(B[4]), .Cin(carry[3]), .Cout(carry[4]), .S(Y[4]) );
+  fullAdder A5(.A(A[5]), .B(B[5]), .Cin(carry[4]), .Cout(carry[5]), .S(Y[5]) );
+  fullAdder A6(.A(A[6]), .B(B[6]), .Cin(carry[5]), .Cout(carry[6]), .S(Y[6]) );
+  fullAdder A7(.A(A[7]), .B(B[7]), .Cin(carry[6]), .Cout(carry[7]), .S(Y[7]) );
+  
+  assign C = carry[7];
+  assign V = A&B&(~C) | (~A&~B&C);
+  
+  
 endmodule
